e2f9d0ec2af29a64eebbf51a6609434925aa0c4a
ayu-010 <aj3157182@gmail.com>
2024-08-22 17:09:24 +0530

    doneeee

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     reactStrictMode: false,
-
-// };
-
-// export default nextConfig;
-
-
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
+    reactStrictMode: false,
+
 };
 
 export default nextConfig;
+
 
