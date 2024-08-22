42b98e3bde71e96c0374aeb3c3b75ac59e3b8f7d
ayu-010 <aj3157182@gmail.com>
2024-08-22 16:50:56 +0530

    gitcongi mjs chnaged

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: false,
+
+// };
+
+// export default nextConfig;
+
+
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-
+  reactStrictMode: false,
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
+
